5bec7b6cddda4f01b5c7de8b54817d965f854df8
Norihito Mizobata <bluesky@mizobata.com>
2025-01-28 18:32:27 +0900

    .ipynb_checkpointsフォルダを管理から外す。

--- .ipynb_checkpoints/jupyternotebook_start-checkpoint.bat
+++
@@ -1,1 +0,0 @@
-%windir%\System32\cmd.exe /K "C:\Users\fx22228.DC00\AppData\Local\miniconda3\Scripts\activate.bat C:\Users\fx22228.DC00\AppData\Local\miniconda3 & activate notebook & jupyter notebook"
